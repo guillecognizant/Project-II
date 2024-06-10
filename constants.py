8aecdae8f691d2a98af20ae5ce1c3f08be6f588b
Guillermo <guillermoandres.hernandezbello@cognizant.com>
2024-06-10 15:37:19 +0000

    Refactoring columns

--- constants.py
+++ constants.py
@@ -5,6 +5,13 @@ PATH = "projectII_alvaroG-GuillermoH/data/Real_Estate.csv"
 
 #Database columns
 cols = ['House age', 'Distance to the nearest MRT station', 'Number of convenience stores', 'Latitude', 'Longitude', 'House price of unit area']
+date_col = 'Transaction date'
+house_age_col = 'House age'
+mtr_dist_col = 'Distance to the nearest MRT station'
+convin_str_col = 'Number of convenience stores'
+lat_col = 'Latitude'
+lon_col = 'Longitude'
+price_col = 'House price of unit area'
 
 
 
