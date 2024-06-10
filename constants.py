82a80a39138195249063d1c406d33825942bd8c9
Guillermo <guillermoandres.hernandezbello@cognizant.com>
2024-06-10 14:58:41 +0000

    Null section rename and part of the refactoring of main

--- constants.py
+++ constants.py
@@ -1,2 +1,10 @@
-a = 100
-path = "0. Projects/2/projectII_alvaroG-GuillermoH/data/Real_Estate.csv"
+
+# File Path
+PATH_G = "0. Projects/2/projectII_alvaroG-GuillermoH/data/Real_Estate.csv"
+PATH = "projectII_alvaroG-GuillermoH/data/Real_Estate.csv"
+
+#Database columns
+cols = ['House age', 'Distance to the nearest MRT station', 'Number of convenience stores', 'Latitude', 'Longitude', 'House price of unit area']
+
+
+
